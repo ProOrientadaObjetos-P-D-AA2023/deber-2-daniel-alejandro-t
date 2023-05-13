3b8377b8752b425c1e22c547e587fb90699f7109
Daniel Tinizaray <danieltinizaray@gmail.com>
2023-05-13 09:11:32 -0500

    Ejercicios python 2da parte

--- ejemplos/mis_clases.py
+++ ejemplos/mis_clases.py
@@ -3,11 +3,47 @@
 """
 
 # Crear dos clases en Python
+
+        
 # Usted defina el nombre y los atributos
 # Puede usar las mismas clases usadas en Java en los ejemplos estudiados
 
 # clase 01
 
+class Asignatura:
+    nombre = ""
+    codigo = ""
+    creditos = 0
+    def __init__(self, nombre, codigo, creditos):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.creditos = creditos
 
+    def __str__(self):
+        return "Asignatura: " + self.nombre + " Codigo: " + self.codigo + " Creditos: " + str(self.creditos)
+
+    def obtener_nombre(self):
+        return self.nombre
+
+    def obtener_codigo(self):
+        return self.codigo
+
+    def obtener_creditos(self):
+        return self.creditos
+
+    def establecer_nombre(self, nombre):
+        self.nombre = nombre
+
+    def establecer_codigo(self, codigo):
+        self.codigo = codigo
+
+    def establecer_creditos(self, creditos):
+        self.creditos = creditos
 
 # clase 02
+class AsignaturaComputacion(Asignatura):
+    nombre_docente = ""
+
+    def __init__(self, nombre, codigo, creditos, lenguaje, nombre_docente):
+        super().__init__(nombre, codigo, creditos)
+        self.lenguaje = lenguaje
--- ejemplos/run2.py
+++ ejemplos/run2.py
@@ -5,14 +5,27 @@
 
 # objeto 01
 
+# Creamos el objeto de la clase Asignatura
+asignatura = Asignatura("Fundamentos de bases de datos", "BD-123", 4)
+
 # crear
 
 # Presentar objeto; usar el método __st__
+print(asignatura)
 
 # objeto 02
 
-# crear ingresando valores por teclado
+# crear ingresando valores por teclado del objeto de la clase AsignaturaComputacion
+# input de los atributos de la clase Asignatura
+nombre = input("Ingrese el nombre de la asignatura: ")
+codigo = input("Ingrese el codigo de la asignatura: ")
+creditos = int(input("Ingrese el numero de creditos de la asignatura: "))
+lenguaje = input("Ingrese el lenguaje de programacion de la asignatura: ")
+nombre_docente = input("Ingrese el nombre del docente de la asignatura: ")
 
+mi_nueva_asignatura = AsignaturaComputacion(nombre, codigo, creditos, lenguaje, nombre_docente)
 # Presentar objeto; usar el método __st__
+
+print(mi_nueva_asignatura)
 
 
